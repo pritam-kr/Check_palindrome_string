6f3850e77731e845d0b557335e4decab7974c660
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-07-28 23:42:24 +0530

    s

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
- const btnCheck = document.querySelector('button');
- const str = document.querySelector('input');
+const btnCheck = document.querySelector("button");
+const str = document.querySelector("input");
 
- btnCheck.addEventListener('click', (e) =>{
-     e.preventDefault();
+btnCheck.addEventListener("click", (e) => {
+  e.preventDefault();
 
-     const inputWord = str.value.toLocaleLowerCase();
-     const string = inputWord.split('');
-     // reverse array 
-     const reArray = string.reverse()
-     // join reverse array 
-     const joinArray = reArray.join('')
-     
-     if(inputWord == joinArray){
-        document.querySelector('.output-text').innerHTML = `<div>
+  const inputWord = str.value.toLocaleLowerCase();
+  const string = inputWord.split("");
+  // reverse array
+  const reArray = string.reverse();
+  // join reverse array
+  const joinArray = reArray.join("");
+
+  if (inputWord == joinArray) {
+    document.querySelector(".output-text").innerHTML = `<div>
         <p> ${inputWord} = ${joinArray} </p>
         <p style="font-size: 1rem;">It is palindrome.</p>
-        </div>`
-        
-     }else{
-        document.querySelector('.output-text').innerHTML = `<p style="font-size: 1rem;">It is not a palindrome.</p>`
-     }
- })
+        </div>`;
+        str.value = ""
+  } else {
+    document.querySelector(
+      ".output-text"
+    ).innerHTML = `<p style="font-size: 1rem;">It is not a palindrome.</p>`;
+  }
+});
